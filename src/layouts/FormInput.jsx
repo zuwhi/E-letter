0de3c6e44e29122bd0fcece79e7ffd1d7dad77ab
zuwhi <zuwhyyy@gmail.com>
2023-12-02 02:26:33 +0700

    deploy

--- src/layouts/FormInput.jsx
+++ src/layouts/FormInput.jsx
@@ -1,6 +1,9 @@
-const FormInput = ({ dataSurat,setSurat, setImg, children, type,formik }) => {
+const FormInput = ({ dataSurat, setSurat, setImg, children, type, formik }) => {
   function reset() {
-    setSurat({});
+    const defaultTemplate = {
+      pembuka: "Sehubungan dengan adanya Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, deserunt? Alias maiores architecto.",
+    };
+    setSurat(defaultTemplate);
     formik.resetForm();
     localStorage.removeItem(dataSurat);
     // localStorage.removeItem("imageIzin");
@@ -10,7 +13,7 @@ const FormInput = ({ dataSurat,setSurat, setImg, children, type,formik }) => {
     <div className="col-span-5 md:h-full min-h-screen md:px-10 px-3 w-full  bg-slate-100 flex flex-col">
       <div className="w-full flex my-4 justify-between">
         <div className="flex items-center">
-          <h1 className="md:text-2xl font-bold text-pallete ">Lengkapi Formulir Surat {type||''}</h1>
+          <h1 className="md:text-2xl font-bold text-pallete ">Lengkapi Formulir Surat {type || ""}</h1>
         </div>
         <div className=" flex justify-end gap-2 items-center">
           <button
